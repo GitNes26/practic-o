d56c451516018c63bf6bf4b246570476a7366a43
GitNes26 <nestorpuentesin@gmail.com>
2022-11-11 12:40:35 -0600

    Se sustituyeron cdns de plugins

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Home</title>
+
+   @extends('templates/links')
+
+</head>
+<body>
+   <div class="container">
+      <div class="row">
+         <div class="card">
+            <div class="input-icon">
+               <input type="search" name="input_search" id="input_search" class="form-control" placeholder="que buscas?">
+               <span><i class="fa-light fa-magnifying-glass"></i></span>
+            </div>
+         </div>
+      </div>
+   </div>
+</body>
+</html>
